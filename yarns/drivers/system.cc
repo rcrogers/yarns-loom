8860c503e2edaef908e2854b5609bf629206b5bc
rcrogers <1903806+rcrogers@users.noreply.github.com>
2024-07-29 09:36:58 -0400

    Backport high-res envelope, without overclocking or extra paraphonic voice (audio bad)

--- yarns/drivers/system.cc
+++ yarns/drivers/system.cc
@@ -32,69 +32,10 @@
 
 namespace yarns {
 
-// http://blog.tkjelectronics.dk/2010/02/stm32-overclocking/
-void RCC_Configuration(void)
-{
-  /* RCC system reset(for debug purpose) */
-  RCC_DeInit();
-
-  /* Enable HSE */
-  RCC_HSEConfig(RCC_HSE_ON);
-
-  /* Wait till HSE is ready */
-  ErrorStatus HSEStartUpStatus = RCC_WaitForHSEStartUp();
-
-  if(HSEStartUpStatus == SUCCESS)
-    {
-    /* Enable Prefetch Buffer */
-    FLASH_PrefetchBufferCmd(FLASH_PrefetchBuffer_Enable);
-
-    /* Flash 2 wait state */
-    FLASH_SetLatency(FLASH_Latency_2);
-
-    /* HCLK = SYSCLK */
-    RCC_HCLKConfig(RCC_SYSCLK_Div1);
-
-    /* PCLK2 = HCLK */
-    RCC_PCLK2Config(RCC_HCLK_Div1);
-
-    /* PCLK1 = HCLK/2 */
-    RCC_PCLK1Config(RCC_HCLK_Div2);
-
-    /* PLLCLK = 8MHz * 9 = 72 MHz */
-    //RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_9);
-    /* PLLCLK = 8MHz * 16 = 128 MHz */
-    RCC_PLLConfig(RCC_PLLSource_HSE_Div1, RCC_PLLMul_16);
-    // The frequency has also been changed in system_stm32f10x
-
-    /* Enable PLL */
-    RCC_PLLCmd(ENABLE);
-
-    /* Wait till PLL is ready */
-    while(RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
-        {;}
-
-    /* Select PLL as system clock source */
-    RCC_SYSCLKConfig(RCC_SYSCLKSource_PLLCLK);
-
-    /* Wait till PLL is used as system clock source */
-    while(RCC_GetSYSCLKSource() != 0x08)
-        {;}
-    }
-
-  // /* Enable peripheral clocks --------------------------------------------------*/
-  // /* Enable GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOF, GPIOG and AFIO clocks */
-  // RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB |RCC_APB2Periph_GPIOC
-  //       | RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOE | RCC_APB2Periph_GPIOF | RCC_APB2Periph_GPIOG
-  //       | RCC_APB2Periph_AFIO, ENABLE);
-}
-
 void System::Init() {
   SystemInit();
   
   NVIC_SetVectorTable(NVIC_VectTab_FLASH, 0x1000);
-
-  RCC_Configuration();
 
   RCC_APB2PeriphClockCmd(
       RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC |
